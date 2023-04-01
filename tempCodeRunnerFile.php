2aea6fafc082fe7f2974ba4a80d80dec8cca59e3
Mohamed Lamine <boumaza951@gmail.com>
2023-04-01 17:54:29 +0100

    commit

---
+++ tempCodeRunnerFile.php
@@ -0,0 +1,9 @@
+<?php
+$host = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "client";
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
