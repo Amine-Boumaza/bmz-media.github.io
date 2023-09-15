5aae46d6e92a6aaca79346f5f6074e98609bca73
Mohamed Lamine <boumaza951@gmail.com>
2023-09-15 19:38:29 +0100

    commit

--- code/connect.php
+++ code/connect.php
@@ -4,7 +4,7 @@ $host = "127.0.0.1";
 $username = "root";
 $password = "";
 $dbname = "client";
-$conn = mysqli_connect($host, $username, $password, $dbname);
+$conn->connect($host, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
